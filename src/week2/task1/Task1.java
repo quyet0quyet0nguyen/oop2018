80105b05877d8ed3dd3e3893262fb6d585901cff
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-09-19 20:01:03 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,39 @@
 package week2.task1;
 
 public class Task1 {
-
-    public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+public class task1 {
+	public static void main(String args[]) {
+		// find UCLN 
+		System.out.println("UCLN(1,0) = "+gcd(1,0));
+		System.out.println("UCLN(50,30) = "+gcd(50,30));
+		System.out.println("UCLN(0,0) = "+gcd(0,0));
+		// find fibonacci
+		System.out.println("fibonacci(0) = "+fibonacci(0));
+		System.out.println("fibonacci(1) = "+fibonacci(1));
+		System.out.println("fibonacci(40) = "+fibonacci(40));
+	}
+	public static int gcd(int a, int b) {
+        if (a==0&&b==0) return -1;
+        else if (a==0) return b;
+        else if (b==0) return a;
+        else {
+        	while (a!=b) if (a>b) a=a-b; else b=b-a;
+        	return a;
+        }
     }
 
     public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+    	if (n == 0) return 0;
+    	else if (n == 1) return 1;
+    	else {
+    		int memory1 = 1, memory2 = 0,sum = 0;
+    		for (int i=2;i<=n;i++) {
+    			sum = memory1 + memory2;
+    			memory2 = memory1;
+    			memory1 = sum;
+    		}
+    		return sum;
+    	}
     }
+}
 }
