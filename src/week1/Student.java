1f1c88712b7e082c475981c9c71d9fad3274be03
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-09-19 16:49:40 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,54 +1,87 @@
 package week1;
 public class Student {
-    private String name;
-    private String id;
-    private String group;
-    private String email;
-    public Student(){
-        name = "Student";
-        id = "000";
-        group = "INT22041";
-        email = "uet@vnu.edu.vn";
-    }
-    public Student(String n, String sid, String em){
-        name = n;
-        id = sid;
-        email = em;
-    }
-    public Student(Student s){
-        name = s.name;
-        id = s.id;
-        group = s.group;
-        email = s.email;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String n){
-        name = n;
-    }
-    public String getId(){
-        return id;
-    }
-    public void setId(String i){
-        id = i;
-    }
-    public String getGroup(){
-        return group;
-    }
-    public void setGroup(String g){
-        group = g;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String e){
-        email = e;
-    }
-    public void getInfo(){
-         System.out.print("name :" +name+"; ");
-        System.out.print("id :"+id+"; ");
-        System.out.print("group :"+group+"; ");
-        System.out.println("email :"+email+"; ");
-    }
+	private String name;
+	private String id;
+	private String group;
+	private String email;
+
+	public Student() {
+		name = "Student";
+		id = "000";
+		group = "INT22041";
+		email = "uet@vnu.edu.vn";
+	}
+
+	public Student(String n, String sid, String em) {
+		if (n != null)
+			name = n;
+		else
+			name = "unKnow name";
+		if (sid != null)
+			id = sid;
+		else
+			id = "unKnow id";
+		if (em != null)
+			email = em;
+		else
+			email = "unKnow email";
+	}
+
+	public Student(Student s) {
+		name = s.name;
+		id = s.id;
+		group = s.group;
+		email = s.email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String n) {
+		if (n != null)
+			name = n;
+		else
+			name = "unKnow name";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String i) {
+		if (i != null)
+			id = i;
+		else
+			id = "unKnow id";
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String g) {
+		if (g != null)
+			group = g;
+		else
+			group = "unKnow Group";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String e) {
+		if (e != null)
+			email = e;
+		else
+			email = "unKnow email";
+	}
+
+	public void getInfo() {
+		System.out.print("name :" + name + " \t");
+		System.out.print("id :" + id + " \t");
+		System.out.print("group :" + group + " \t");
+		System.out.println("email :" + email + " \t");
+	}
 }
