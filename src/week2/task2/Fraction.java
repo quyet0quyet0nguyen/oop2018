058c5fdac930052952ec6eddfaa2c8f3229841c4
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-09-26 20:10:43 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,15 +1,31 @@
 package week2.task2;
 
+import java.lang.Math;
+
 public class Fraction {
 	private int numerator;
 	private int denominator;
 	
 	public static void main (String args[]) {
-		Fraction fra1 = new Fraction(5,6);
-		Fraction fra2 = new Fraction(25,30);
+		Fraction fra1 = new Fraction(-5,6);
+		Fraction fra2 = new Fraction(7,25);
 		Fraction fra3 = new Fraction(0,1);
-		fra3 = fra1.multiply(fra2);
+		fra3 = fra1.add(fra2);
+		System.out.print("-5/6 + 7/25 = ");
 		fra3.getFraction();
+		fra3 = fra1.subtract(fra2);
+		System.out.print("-5/6 - 7/25 = ");
+		fra3.getFraction();
+		fra3 = fra1.multiply(fra2);
+		System.out.print("-5/6 * 7/25 = ");
+		fra3.getFraction();
+		fra3 =new Fraction(0,1);
+		fra2=fra1.multiply(fra3);
+		System.out.print("-5/6 * 0 = ");
+		fra2.getFraction();
+		fra2=fra1.divide(fra3);
+		System.out.print("-5/6 / 0 = ");
+		fra2.getFraction();
 	}
 	
     public Fraction(int numerator, int denominator) {
@@ -28,19 +44,23 @@ public class Fraction {
     
     public static Fraction simplify(Fraction fra) {
     	boolean task = true;
-    	if (fra.numerator!=0) {
-    	if (fra.numerator*fra.denominator<0) task = false;
-    	fra.numerator = Math.abs(fra.numerator);
-    	fra.denominator = Math.abs(fra.denominator);
-    	int a = fra.numerator;
-    	int b = fra.denominator;
-        while (a!=b) if (a>b) a=a-b; else b=b-a;
-        fra.numerator /= a;
-        fra.denominator /= b;
-        if (task == false ) {
-        	fra.numerator = -fra.numerator;
-        } 
-    	}
+    	if (fra.denominator!=0) {
+    		if (fra.numerator!=0) {
+    			if (fra.numerator*fra.denominator<0) task = false;
+    			fra.numerator = Math.abs(fra.numerator);
+    			fra.denominator = Math.abs(fra.denominator);
+    			int a = fra.numerator;
+    			int b = fra.denominator;
+    			while (a!=b) if (a>b) a=a-b; else b=b-a;
+    			fra.numerator /= a;
+    			fra.denominator /= b;
+    			if (task == false ) {
+    				fra.numerator = -fra.numerator;
+    			} 
+    		} else {
+    		fra.denominator=1;
+    		}
+    	} 
         return fra;
     }
 
