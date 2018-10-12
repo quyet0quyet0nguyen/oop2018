daee693e343308194ffbee7163a5c25d880ca4b5
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-10-12 08:30:02 +0700

    Update package-info.java

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -7,3 +7,34 @@
  * @version 0.1
  */
 package week4.task1;
+public class HoaQua {
+	protected String shape;
+	protected String color;
+	public HoaQua() {
+		shape = "shape";
+		color = "red";
+	}
+	public HoaQua(String shape,String color) {
+		this.shape = shape;
+		this.color = color;
+	}
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+	public String getShape() {
+		return shape;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String toString() {
+		return (" shape : "+shape+" color : "+color);
+	}
+	public static void main(String[] args) {
+		HoaQua hoaqua = new HoaQua();
+		System.out.println(hoaqua);
+	}
+}
