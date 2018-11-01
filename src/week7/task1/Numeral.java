a4c6114006da0ee138141b2e5b843dbe29619689
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-11-01 22:48:26 +0700

    Update Numeral.java

--- src/week7/task1/Numeral.java
+++ src/week7/task1/Numeral.java
@@ -1,4 +1,23 @@
 package week7.task1;
 
-public class Numeral {
+public class Numeral extends Expression{
+	protected int value;
+	
+	public Numeral(int value) {
+		this.value = value;
+	}
+	
+	public Numeral() {
+		value = 0;
+	}
+	
+	@Override
+	public String toString() {
+		return (""+this.value);
+	}
+
+	@Override
+	public int evaluate() {
+		return this.value;
+	}
 }
