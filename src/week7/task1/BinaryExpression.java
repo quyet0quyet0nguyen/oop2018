e7f384991d10954f2fe970da4a0bc5c33ee67d2f
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-11-01 22:44:54 +0700

    Update BinaryExpression.java

--- src/week7/task1/BinaryExpression.java
+++ src/week7/task1/BinaryExpression.java
@@ -1,7 +1,32 @@
 package week7.task1;
 
-public abstract class BinaryExpression extends Expression {
+public class Division extends BinaryExpression{
+	protected Expression left;
+	protected Expression right;
 	
-	public abstract Expression left ();
-	public abstract Expression right ();
+	public Division(Expression E1,Expression E2) {
+		this.left = E1;
+		this.right = E2;
+	}
+	
+	@Override
+	public Expression left() {
+		return left;
+	}
+
+	@Override
+	public Expression right() {
+		return right;
+	}
+
+	@Override
+	public String toString() {
+		return ("("+left.toString()+") /"+right.toString());
+	}
+
+	@Override
+	public int evaluate() {
+		return left.evaluate()/right.evaluate();
+	}
+
 }
