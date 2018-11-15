63f124cb6cb37665db87f579f12c8c7e5f1260a8
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-11-15 23:05:13 +0700

    Update Task2.java

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,41 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+	public static void main(String args[]) {
+		float[] a = new float[1000] ;
+		Random rand = new Random();
+		for (int i=0;i<1000;i++) {
+			a[i] = rand.nextFloat()*1000 ;
+		}
+		sort(a);
+		for (int i=0;i<1000;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+	public static void sort(float[] input) {
+
+        int inputLength = input.length;
+        float temp;
+        boolean is_sorted;
+
+        for (int i = 0; i < inputLength; i++) {
+
+            is_sorted = true;
+
+            for (int j = 1; j < (inputLength - i); j++) {
+
+                if (input[j - 1] > input[j]) {
+                    temp = input[j - 1];
+                    input[j - 1] = input[j];
+                    input[j] = temp;
+                    is_sorted = false;
+                }
+
+            }
+            if (is_sorted) break;
+        }
+        
+    }
 }
