988a26243673c52c818056b7d2a85ff311e94633
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-11-23 08:11:48 +0700

    Update Task2.java

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,20 @@
 package week11;
-
+import java.util.ArrayList;
 public class Task2 {
+	public static <T extends Comparable> T Min(ArrayList<T> a) {
+		T min=a.get(0);
+		for (T i: a) {
+			if (min.compareTo(i)>0) min = i;
+		}
+		return min;
+	}
+	public static void main(String args[]) {
+		ArrayList <Integer> a = new ArrayList();
+		a.add(1);
+		a.add(3);
+		a.add(2);
+		a.add(100);
+		System.out.println(Min(a));
+		
+	}
 }
