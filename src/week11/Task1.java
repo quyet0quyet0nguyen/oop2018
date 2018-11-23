7288acd404a4d34049fcf984dcb287a3f0d5141b
quyet0quyet0nguyen <43262313+quyet0quyet0nguyen@users.noreply.github.com>
2018-11-23 08:12:06 +0700

    Update Task1.java

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,23 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task1 {
+	public static <T extends Comparable> ArrayList sapxep(ArrayList<T> a) {
+		T c;
+		a.sort(null);
+		return a;
+	}
+	public static void main(String args[]) {
+		ArrayList <Integer> a = new ArrayList();
+		a.add(1);
+		a.add(3);
+		a.add(2);
+		a.add(0);
+		sapxep(a);
+		for (int i:a) {
+			System.out.println(a.get(i));
+		}
+		
+	}
 }
